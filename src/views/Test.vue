80b260183c3e9d42ceadfe675d71f293e980999f
along0628 <506570320@qq.com>
2020-05-28 18:36:58 +0800

    day 01 , create vue

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>这是我的第一个 Vue 页面</h1>
+        <h2>6666</h2>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button loading type="info" loading-text="加载中..." />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: green;
+    }
+</style>
