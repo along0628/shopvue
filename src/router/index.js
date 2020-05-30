4d749d7e0150978b77a001d79f90d61ee0e70782
along0628 <506570320@qq.com>
2020-05-30 16:33:22 +0800

    vue first finish(will be update).

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../views/Test'
+import AddressList from '../views/AddressList'
+import AddressNew from '../views/AddressNew'
+import AddressEdit from '../views/AddressEdit'
+import Detail from '../views/Detail'
+import Success from '../views/Success'
+import Info from '../views/Info'
 
 Vue.use(VueRouter)
 
@@ -23,7 +29,37 @@ Vue.use(VueRouter)
     path:'/test',
     name:'测试',
     component:Test
-  }
+  },
+  {
+    path:'/addressList',
+    name:'地址列表',
+    component:AddressList
+  },
+  {
+     path:'/addressNew',
+     name:'新增地址',
+     component:AddressNew
+  },
+  {
+     path:'/addressEdit',
+     name:'新增地址',
+     component:AddressEdit
+  },
+    {
+      path:'/detail',
+      name:'商品详情页面',
+      component:Detail
+    },
+    {
+      path:'/success',
+      name:'订单支付成功',
+      component:Success
+    },
+    {
+      path:'/info',
+      name:'完成Info页面',
+      component:Info
+    }
 ]
 
 const router = new VueRouter({
